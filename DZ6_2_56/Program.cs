657987761b03ffc65595339d3ea1435d84ea3245
Maikl <umnov.mixail@mail.ru>
2023-09-13 21:05:33 +0400

    Домашняя работа

---
+++ DZ6_2_56/Program.cs
@@ -0,0 +1,144 @@
+﻿
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            System.Console.Write($"{array[i, j],3}    ");
+        System.Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)
+{
+    // int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+        //     for (int j = 0; j < array.GetLength(1); j++)
+
+
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+            for (int l = 0; l < array.GetLength(1) - 1; l++)
+
+                if (array[i, l] < array[i, l + 1])
+
+                    (array[i, l + 1], array[i, l]) = (array[i, l], array[i, l + 1]);
+                            temp = array[i, j];
+                        array[i, j] = array[i, l+1];
+                        array[i, l+1] = temp;
+
+                    }
+                        System.Console.WriteLine(array[i,j]);
+                }
+
+
+
+
+}
+
+System.Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+SortArray(array);
+PrintArray(array);
+
+
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            System.Console.Write($"{array[i, j],3}    ");
+        System.Console.WriteLine();
+    }
+}
+
+int[] FindSums(int[,] array)
+{
+    int[] sums = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            sums[i] += array[i, j];
+    System.Console.Write($"{sums[i]}   ");        
+    }
+    return sums;
+}
+
+void FindMin(int[]sums)
+{
+    int indexMin = 0;
+    for (int i = 0; i < sums.Length; i++)
+    {
+        if (sums[i] < sums[indexMin])
+        indexMin = i;
+    }System.Console.WriteLine($" ответ - {indexMin + 1} ");
+}
+
+
+System.Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+int[]sums = FindSums(array);
+FindMin(sums);
+
+// PrintArray(array);
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
